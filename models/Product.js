4b6972212ba92e0532a50f76b170f5f3ebb4cb5d
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-11-02 19:30:23 -0400

    updated the columns in Products.js

--- models/Product.js
+++ models/Product.js
@@ -15,6 +15,37 @@ Product.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          msg: "Price must be a valid decimal"
+        }
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: {
+          msg: 'Stock must be a valid numeric'
+        }
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
