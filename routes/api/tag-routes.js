d340b73ef687cec7f60acb8855f2d9b47122b1d2
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-11-10 13:34:23 -0500

    added the get all for tags and the get for single id coding

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,15 +2,26 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  const tagData = await Tag.findAll({
+    include: {model:Product},
+  }).catch((err) => {
+    res.json(err);
+  });
+    res.json(tagData);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const tagData = await Tag.findByPk(req.params.id, {
+    include: {model: Product},
+  }).catch((err) => {
+    res.json(err);
+  });
+    res.json(tagData);
 });
 
 router.post('/', (req, res) => {
